fdbe8b0983a0c9b0dd78af225a6acdc7af1118ff
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-04-13 23:31:31 +0300

    bump versionCode to 3 and versionName to 1.0.3 for release updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ru.housekpr.gate"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
