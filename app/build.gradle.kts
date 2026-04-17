76ea2971fe62ce31c29986ca7b1a24ac93027ccf
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-04-17 13:48:48 +0300

    new version 7. remove vk add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ru.housekpr.gate"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
